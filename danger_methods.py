45fabc508e4ff98bc45245479afd0bbd176902a5
edoardesd <edoardesd@gmail.com>
2017-02-14 13:01:10 +0100

    Metodi per calcolare pericolo path e finito parse file

---
+++ danger_methods.py
@@ -0,0 +1,105 @@
+#Parsa il file, occhio che ritorna 4 valori questa volta, l'ultimo e' il danger
+def parse_dat_file(dat_file):
+	file_dat = np.genfromtxt(file, delimiter='\n', dtype=None)
+
+	n = int(file_dat[1][11:]) #parse param n: dimension of array
+
+	ALPHA = float(file_dat[3][15:]) #parse param alpha
+	file_dat = file_dat[5:]
+
+	raw_x = []
+	raw_y = [] 
+	raw_d = []
+
+	#start split coord x in vector raw_x and idem for y
+	for row in file_dat:
+		if "coordX" in row:
+			isX = True
+			isY = False
+			isD = False
+		if "coordY" in row:
+			isX = False
+			isY = True
+			isD = False
+		if "d [*,*]" in row:
+			isY = False
+			isD = True
+
+		if isX:
+			raw_x.append(" ".join(row.split()))
+
+		if isY:
+			raw_y.append(" ".join(row.split()))
+
+		if isD: 
+			raw_d.append(" ".join(row.split()))
+
+
+	
+	#delete initial words and final semicolumn
+	raw_x.pop(0)
+	raw_x.pop(len(raw_x)-1)
+	raw_y.pop(0)
+	raw_y.pop(len(raw_y)-1)
+	raw_d.pop(0)
+	raw_d.pop(0)
+	raw_d.pop(len(raw_y))
+
+	raw_d =' '.join(raw_d)
+	raw_d = raw_d.split(' ')
+
+	raw_x =' '.join(raw_x)
+	raw_x = raw_x.split(' ')
+	raw_y =' '.join(raw_y)
+	raw_y = raw_y.split(' ')
+	
+	
+
+	#transfer vector raw_x in a dictionary. key=index, value=coordX
+	i=0
+	for column in raw_x:
+		if i%2 == 0:
+			even = int(column)
+		if i%2 != 0:
+			coord_x[even] = int(column)
+
+		i = i+1
+
+	#transfer vector raw_y in a dictionary. key=index, value=coordY
+	i=0
+	for column in raw_y:
+		if i%2 == 0:
+			even = int(column)
+		if i%2 != 0:
+			coord_y[even] = int(column)
+
+		i = i+1
+
+	#transfer raw_d in a matrix
+	row = []
+	danger = []
+	for i in range (0, len(raw_d)+1):
+		if (i%(n+2)) != 0:
+			row.append(float(raw_d[i]))
+		else:
+			if i != 0:
+				danger.append(row)
+			row = []
+	
+   	
+
+	#possibile ottimizzare le fusione in un unico dizionario, anche piu sopra
+	#merge the two dictionaries
+	coord = defaultdict(list)
+	for k, v in chain(coord_x.items(), coord_y.items()):
+    		coord[k].append(v)
+	
+	return n, ALPHA, coord, danger
+
+#calcola il pericolo di un path
+def compute_danger(my_path):
+	path_danger = 0
+	for i in range(0, len(my_path)-1):
+		path_danger = path_danger + danger[my_path[i]][my_path[i+1]]
+
+	return path_danger
